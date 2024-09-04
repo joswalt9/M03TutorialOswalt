b054b2720ba7ee59fc4a018343f17ac6f510b9e2
joswalt9 <156866636+joswalt9@users.noreply.github.com>
2024-09-03 21:54:31 -0400

    Add files via upload
    Tutorial Completion

---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ["Yoshi", "Ryu", "Chun-li", "Mario"];
+const ages = [20, 25, 30, 35];
+
+console.log(people);
+
+module.exports = {
+  people,
+  ages,
+};
---
+++ test.js
@@ -0,0 +1,10 @@
+const name = "Jacob";
+
+console.log(name);
+
+const greet = (name) => {
+  console.log(`hello, ${name}`);
+};
+
+greet("Jacob");
+greet("Yoshi");
